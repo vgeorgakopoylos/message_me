55645307f77681a1fe6a3870e8dd9a5fdeb06434
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-03-27 13:00:41 -0700

    added login functionality and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'chatrooms#index'
   get 'chatrooms', to: 'chatrooms#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
